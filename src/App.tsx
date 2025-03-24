b9b25e80e0ea5adfc09d88a7cb83aa56fac067ef
mr-developer01 <rahulpratik1212@gmail.com>
2025-03-24 15:55:32 +0530

    initial setups

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { CssBaseline } from "@mui/material";
+import CollapsibleTable from "./ui/CollapsibleTable";
+
+const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <CollapsibleTable />
+    </>
+  );
+};
+
+export default App;
